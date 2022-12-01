878a05dcf217efbaabff1510d5ac31dd451ddd5d
estefaniacr <estefania.c.r@hotmail.com>
2022-12-01 01:16:32 +0100

    Eclint corrections

--- src/components/series.item/item.ts
+++ src/components/series.item/item.ts
@@ -1,5 +1,4 @@
 import { Component } from '../component/component.js';
-//import { SERIES } from '../../mocks/series.js';
 import { SerieType } from '../../models/serie.js';
 import { serieScore } from '../series.state/state.js';
 
@@ -13,8 +12,7 @@ export class Item extends Component {
     private createTemplate(series: Array<SerieType>) {
         let itemsTemplate = '';
         series.forEach((item) => {
-            itemsTemplate += `
-             <li class="serie">
+            itemsTemplate += `<li class="serie">
                     <img
                         class="serie__poster"
                         src="${item.poster}"
